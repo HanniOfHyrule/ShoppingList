08a126d5f56023eba2f1e2ba801d386c6a6fa53d
Hanni Liebler <48068241+code-knackerin@users.noreply.github.com>
2021-08-23 11:25:36 +0200

    testing Input

--- src/Input.js
+++ src/Input.js
@@ -1,4 +1,4 @@
-import { name } from "file-loader";
+//import { name } from "file-loader";
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
@@ -14,7 +14,10 @@ const initialList = [
 ];
 
 const App = () => {
-  const [list, dispatchList] = React.useReducer(listReducer, initialList);
+  const [listData, dispatchListData] = React.useState(listReducer, {
+    list: initialList,
+    isShowList: true,
+  });
   const [name, setName] = React.useState("");
 
   function handleChange(event) {
@@ -22,14 +25,15 @@ const App = () => {
   }
 
   function handleAdd() {
-    dispatchList({ type: "ADD_ITEM", name, id: uuidv4() });
+    dispatchListData({ type: "ADD_ITEM", name, id: uuidv4() });
 
     setName("");
   }
   return (
     <div>
       <AddItem name={name} onChange={handleChange} onAdd={handleAdd} />
-      <List list={list} />
+
+      <List list={listData.list} />
     </div>
   );
 };
@@ -52,8 +56,12 @@ const List = ({ list }) => (
 const listReducer = (state, action) => {
   switch (action.type) {
     case "ADD_ITEM":
-      return state.concate({ name: action.name, id: action.id });
-      defautl: throw new Error();
+      return {
+        ...state,
+        list: state.list.concat({ name: action.name, id: action.id }),
+      };
+    default:
+      throw new Error();
   }
 };
 
